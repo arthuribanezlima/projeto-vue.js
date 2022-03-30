a40e20a17dc944de0cec945e99e895c32562d1ba
arthuribanezlima <arthuribanezlima@outlook.com>
2022-03-29 22:02:05 -0300

    Create app.vue.vue

---
+++ app.vue.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="https://i0.wp.com/terminaldeinformacao.com/wp-content/uploads/2017/11/01-positivo.jpg?ssl=1">
+  <HelloWorld msg="bem vindo ao meu primeiro projeto vue.js"/>
+
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+ }
+</script>
+
+<style>
+ #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #015806;
+  margin-top: 60px;
+ }
+</style>
